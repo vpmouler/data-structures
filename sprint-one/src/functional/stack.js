053f6eadfc30b35b44b71dfe809e9c3ff7d7f303
Seva Mouler <vpmouler@gmail.com>
2017-08-23 15:28:08 -0700

    Functional pattern passes stacks

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,17 +2,29 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {}; // obj[key]=value
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
 
   someInstance.pop = function() {
+    // take out last element
+    var lastElement = storage[Object.keys(storage).length-1];
+    delete storage[Object.keys(storage).length-1];
+    count--;
+    return lastElement;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
 };
+
+var arr = [1,2,67];
+console.log(arr.pop());
