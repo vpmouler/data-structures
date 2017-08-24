755f7f6c51e010a3d7e069ec08dfd16f5c48bc5b
Seva Mouler <vpmouler@gmail.com>
2017-08-24 15:53:29 -0700

    completed linked list

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -17,5 +17,5 @@ Queue.prototype.dequeue = function() {
 };
 
 Queue.prototype.size = function() {
-	return Object.keys(this.storage).length;
+  return Object.keys(this.storage).length;
 };
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,52 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    //if head is null we want to set tail equal
+    // if head not null 
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.tail = newNode;
+      list.head = list.tail;
+      list.head.next = newNode;
+    }
+    list.tail.next = newNode;
+    list.tail = newNode;
+    // head is suppose to be added value
+    // still call list.tail to be NOde(value);
+    // list.head.next = newNode;
+    // list.tail = Node(value);
+    // need reference to head
+    // it is currently replacing list.tail
   };
 
   list.removeHead = function() {
+    delete list.head;
+    list.head = list.tail; //head is the item it points to (tail)
+    return list.head.value;
   };
 
   list.contains = function(target) {
+    var truthy = false;
+    var recurseFind = function(element) {
+    // console.log(element);
+    // base case
+    if ( element.value === target ) {
+      return true;
+    } else if ( element.next === null ) { // we've reached tail
+      // if next is null 
+      return false;
+    } else {
+      return recurseFind(element.next);
+    }
+
+        // return false
+      // else if .next is not null, 
+
+
+    // recursive
+      
+    }
+    return recurseFind(list.head);
   };
 
   return list;
@@ -27,3 +67,28 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+// var list = LinkedList();
+
+//console.log(list);
+
+// list.addToTail(2);
+// list.addToTail(10);
+// list.addToTail(400);
+// list.addToTail('a');
+// console.log(list);
+
+// console.log(list.contains('a'));
+
+
+// console.log('NEXT', list.head.next);
+// console.log('NEXT', list.head.next.next);
+
+// LinkedList(Node(123))
+
+// linkedList.addToTail(4);
+// linkedList.addToTail(5);
+// expect(linkedList.head.value).to.equal(4);
+// linkedList.removeHead();
+// expect(linkedList.head.value).to.equal(5);
