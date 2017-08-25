b9278bc3d7f2e613f092a5b75919ed14f5f6cc83
Seva Mouler <vpmouler@gmail.com>
2017-08-25 15:00:38 -0700

    completed set, all tests pass

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,25 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if ( !this._storage[item] ) {
+  	this._storage[item] = item;
+  };
 };
 
 setPrototype.contains = function(item) {
+	return this._storage[item] !== undefined;
 };
 
 setPrototype.remove = function(item) {
+	delete this._storage[item];
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions? 
  */
